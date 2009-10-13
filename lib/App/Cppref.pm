6c125c065f9087a2f897b77b97428b274fa621b9
Kazuho Oku <kazuhooku@gmail.com>
2009-10-13 18:22:24 +0900

    add lib/App/Cppref.pm so that cppref can be installed using CPAN.pm

---
+++ lib/App/Cppref.pm
@@ -0,0 +1,30 @@
+package App::Cppref;
+
+1;
+__END__
+
+=head1 NAME
+
+App::Cppref - man-style access to cppreference.com documents (using w3m)
+
+=head1 DESCRIPTION
+
+This is a dummy module so that cppref can be installed using cpan, like:
+
+  # cpan -i App::Cppref
+
+Please refere to L<cppref> for how to use the application.
+
+=head1 AUTHOR
+
+Kazuho Oku
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+The documents are from http://www.cppreference.com/ (under Creative Commons Attribution 3.0 license).
+
+=cut
